71818cb4d2e1ff223599dc88b39f6571dbd46dd9
LEE HYUN WOO <lhwda1388@naver.com>
2020-05-24 20:10:33 +0900

    axios timeout setting

--- src/hooks/useGetFetch.ts
+++ src/hooks/useGetFetch.ts
@@ -6,17 +6,22 @@ export function useGetFetch(url: string, params: string): Array<any> {
   const [loaded, setLoaded] = useState<boolean>(false)
 
   const fetchData = useCallback(async () => {
+    const instance = axios.create()
     console.log(`url: ${url}, params: ${params}`)
-    const {
-      data: {
-        data: { results }
-      }
-    } = await axios.get(`${url}?${params}`, {
-      timeout: 10 * 1000
-    })
-    console.log(`result: ${results}`)
-    setResult(results)
-    setLoaded(true)
+    try {
+      const {
+        data: {
+          data: { results }
+        }
+      } = await instance.get(`${url}?${params}`, {
+        timeout: 3 * 1000
+      })
+      console.log(`result: ${results}`)
+      setResult(results)
+      setLoaded(true)
+    } catch (error) {
+      setLoaded(true)
+    }
   }, [url, params])
 
   useEffect(() => {
